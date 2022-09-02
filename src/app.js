2e95133e8efd21f8a202c26c582573d73a477854
elias <eliasmanica@poli.ufrj.br>
2022-09-02 17:38:11 -0300

    verificando se tem nome repetido

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import joi from "joi";
 
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
@@ -15,12 +16,59 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("batePapoUOL");
 });
 
+const nameSchema = joi.object({
+  name: joi.string().required(),
+});
+
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
+  const validation = nameSchema.validate(req.body, { abortEarly: false });
+
+  //VALIDAR SE O NOME JÁ TA PRESENTE
+
+  //   let listName = [];
+
+  //   db.collection("participantsUOL")
+  //     .find()
+  //     .toArray()
+  //     .then((value) => {
+  //       listName = value;
+  //     });
+
+  //   const findName = listName.find((value) => {
+  //     value.name === name;
+  //   });
+
+  //   if (findName) {
+  //     res.sendStatus(409);
+  //     return;
+  //   }
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(errors);
+    return;
+  }
+
   const body = { name: name, lastStatus: Date.now() };
   try {
-    const response = await db.collection("participantsUOL").insertOne(body);
-    res.sendStatus(201);
+    const listUsers = await db.collection("participantsUOL").find().toArray();
+    let findName = false;
+    listUsers.forEach((value) => {
+      if (value.name === name) {
+        findName = true;
+      }
+    });
+    if (findName) {
+      console.log("tem repetido");
+      res.sendStatus(409);
+      return;
+    } else {
+      const response = await db.collection("participantsUOL").insertOne(body);
+
+      res.sendStatus(201);
+      return;
+    }
   } catch (error) {
     res.sendStatus(422);
   }
@@ -31,7 +79,27 @@ app.get("/participants", async (req, res) => {
     const response = await db.collection("participantsUOL").find().toArray();
     res.send(response);
   } catch (error) {
-    res.send("erro");
+    res.sendStatus(500);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { name } = req.body;
+  const body = { name: name, lastStatus: Date.now() };
+  try {
+    const response = await db.collection("messagesUOL").insertOne(body);
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  try {
+    const response = await db.collection("messagesUOL").find().toArray();
+    res.send(response);
+  } catch (error) {
+    res.sendStatus(500);
   }
 });
 
